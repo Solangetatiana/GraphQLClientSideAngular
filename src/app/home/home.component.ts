224b4fb3b37053db4a6e713c3e4589db27c7460c
Solangetatiana <36998666+Solangetatiana@users.noreply.github.com>
2019-07-17 17:38:55 -0300

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,8 @@ import { Apollo, QueryRef } from 'apollo-angular';
 import gql from 'graphql-tag';
 
 const PHOTOS_QUERY = gql`
-  query getPhotos {
-    getPhotos {
+  query getPhotosMock {
+    getPhotosMock {
       albumId
       id
       title
@@ -27,10 +27,32 @@ const PHOTO_QUERY = gql`
 
 `;
 
+const CREATE_PHOTO = gql`
+  mutation createPhotoMock($data:	PhotoInput!) {
+    createPhotoMock(data:	$data)
+    {
+      albumId
+      id
+      title
+      url
+      thumbnailUrl
+    }
+  }
+`;
 
-const CREATE_LINK_MUTATION = gql`
-  mutation createPhotoMock($albumId: ID!, $title: String!, $url: String, $thumbnailUrl: String) {
-    createPhotoMock(albumId: $albumId, title: $title, url: $url, thumbnailUrl: $thumbnailUrl) {
+const DELETE_PHOTO = gql`
+  mutation deletePhotoMock($where: PhotoWhere!) {
+    deletePhotoMock(where:	$where)
+    {
+      id
+    }
+  }
+`;
+
+const UPDATE_PHOTO = gql`
+  mutation updatePhotoMock($data:	PhotoInput!, $where: PhotoWhere!) {
+    updatePhotoMock(data:	$data, where:	$where)
+    {
       albumId
       id
       title
@@ -67,9 +89,57 @@ export class HomeComponent implements OnInit {
     this.query.valueChanges.subscribe(result => {
       console.log (result);
       console.log (result.data);
-      console.log (result.data.getPhotos);
-      this.photos = result.data.getPhotos;
+      console.log (result.data.getPhotosMock);
+      this.photos = result.data.getPhotosMock;
     });
+
+    this.apollo.mutate({
+      mutation: CREATE_PHOTO,
+      variables: {
+        data: {
+              albumId: 4,
+              title: 'teste 4',
+              url: 'teste 4',
+              thumbnailUrl: null
+              }
+      }
+    }).subscribe();
+
+    this.apollo.mutate({
+      mutation: DELETE_PHOTO,
+      variables: {
+        where: {
+              id: 1
+              }
+      }
+    }).subscribe();
+
+    this.apollo.mutate({
+      mutation: UPDATE_PHOTO,
+      variables: {
+        data: {
+          albumId: 5,
+          title: 'teste 5',
+          url: 'teste 5',
+          thumbnailUrl: null
+          },
+        where: {
+              id: 300
+              }
+      }
+    }).subscribe();
+
+    this.query = this.apollo.watchQuery({
+      query: PHOTOS_QUERY
+    });
+
+    this.query.valueChanges.subscribe(result => {
+      console.log (result);
+      console.log (result.data);
+      console.log (result.data.getPhotosMock);
+      this.photos = result.data.getPhotosMock;
+    });
+
   }
 
 
